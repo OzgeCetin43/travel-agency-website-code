ff75f072b2965300a05a315b7d6762c1e4fd4552
Ozge Cetin | Odeon Technology <ozge.cetin@odeontechnology.com>
2023-03-20 18:15:05 +0300

    login page added.

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { FaLinkedinIn, FaFacebookF, FaInstagram } from "react-icons/fa"
+import { NavLink } from 'react-router-dom'
+import "./Login.css"
+
+const Login = () => {
+    return (
+        <div className='login-container'>
+            <div className="login-inner-container">
+                <div className="login-container-content">
+                    <h2 className='login-title'>Welcome to Our Travel Agency Website</h2>
+                    <div>
+                        <FaLinkedinIn />
+                        <FaFacebookF />
+                        <FaInstagram />
+                    </div>
+                </div>
+                <div className="login-area-container">
+                    <div className="form-box login">
+                        <form action="#">
+                            <h2 className='sign-in-title'>Sign In</h2>
+                            <p className='sign-in-description'>Sign in your account</p>
+                            <div className="input-container">
+                                <input type="email" required placeholder='E-mail' />
+                            </div>
+                            <div className="input-container">
+                                <input type="password" placeholder='Password' />
+                            </div>
+                            <div className="remember-me-container">
+                                <div>
+                                    <input type="checkbox" className='login-check-box' />
+                                    <span className='remember-text'>Remember me</span>
+                                </div>
+                                <a href="/travel-agency-website/login" className='forgot-password'>Forgot Password</a>
+                            </div>
+                            <NavLink to="/travel-agency-website/" className="sign-in-button">Sign In</NavLink>
+                            <div className="login-register">
+                                <p>Don't have an account?</p>
+                                <a href='/travel-agency-website/login' className='register-link'> Sign Up</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div >
+        </div >
+    )
+}
+
+export default Login
--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 
 const Routers = () => {
     return (
         <Routes>
             <Route path="/travel-agency-website/" element={<Home />} />
+            <Route path="/travel-agency-website/login" element={<Login />} />
         </Routes>
     );
 }
