aa11f4743e9e1ea07d18dc13f52956ebca073c87
Ozge Cetin | Odeon Technology <ozge.cetin@odeontechnology.com>
2023-03-29 17:23:40 +0300

    about pages changed

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import hero from "../../assets/images/about.png"
+import aboutHero from "../../assets/images/about-hero.jpg"
 import { about } from "../../assets/data/about"
+import { FcApproval } from "react-icons/fc"
 import "./About.css"
 
 const About = () => {
@@ -32,24 +34,48 @@ const About = () => {
                     ))
                 }
             </div>
-            <div className='about-inner-container'>
-                <div className="about-left-container">
-                    <h2 className='about-title'>Fast Facts</h2>
-                    <p className='about-description'>We’re impartial and independent, and every day we create distinctive, world-class programmes and content.</p>
+            <div className='about-content'>
+                <div className="about-content-left">
+                    <h1 className='about-content-title'>Dare to Explore with Travel Agency</h1>
+                    <p className='about-content-description'>We are trusted by our clients and have a reputation for the best services in the field. </p>
+                    <div className="about-inner-content">
+                        <div className='about-content-item'>
+                            <FcApproval /> <span>Quality Services Ensure</span>
+                        </div>
+                        <div className="about-content-item">
+                            <FcApproval /> <span>Customer Care</span>
+                        </div>
+                        <div className="about-content-item">
+                            <FcApproval /> <span>Experience Guides</span>
+                        </div>
+                        <div className="about-content-item">
+                            <FcApproval /> <span>Private Transport</span>
+                        </div>
+                        <div className="about-content-item">
+                            <FcApproval /> <span>Diverse Destinations</span>
+                        </div>
+                        <div className="about-content-item">
+                            <FcApproval /> <span>Great Hotels</span>
+                        </div>
+                        <div className="about-content-item">
+                            <FcApproval /> <span>Fast Booking</span>
+                        </div>
+                        <div className="progress-bar-container">
+                            <h4 className='progress-bar-title'>Best Services</h4>
+                            <div className="progress-bar">
+                                <div className='best-serevices-progress-bar'><span>82%</span></div>
+
+                            </div>
+                            <h4 className='progress-bar-title'>Tour Agents</h4>
+                            <div className="progress-bar">
+                                <div className='tour-agents-progress-bar'><span>63%</span></div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className='about-us-facts'>
-                <div className="fact-item">
-                    <h3 className="fact-item-title">2 million</h3>
-                    <p className='fact-item-description'>Articles have been public around the world</p>
-                </div>
-                <div className="fact-item">
-                    <h3 className="fact-item-title">100,000+</h3>
-                    <p className='fact-item-description'>Registered users account</p>
-                </div>
-                <div className="fact-item">
-                    <h3 className="fact-item-title">350+</h3>
-                    <p className='fact-item-description'>Countries and regions have our presence</p>
+                <div className="about-content-right">
+                    <img src={aboutHero} alt="about us" />
                 </div>
             </div>
         </div>
